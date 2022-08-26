2aca38313af424f8fc3a7edbd8ab8536739752d4
birkan <b.561birkan@gmail.com>
2022-08-26 23:32:01 +0300

    creating first template

--- application/views.py
+++ application/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+# def home(request):
+#     # print(request.user)  # if it doesn't work, first trigger python manage.py migrate
+#     html = "<h1>home response html</h1>"
+#     return HttpResponse(html)
+
+def home(request):
+    return render(request, "application/index.html")
